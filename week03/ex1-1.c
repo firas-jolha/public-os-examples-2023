513bc101cfc31948a4e9ff6ddcb61578e4b07b23
Mahmood Naderi <32930400+mahmood-naderi@users.noreply.github.com>
2023-09-20 10:27:55 +0300

    Update ex1-1.c

--- week03/ex1-1.c
+++ week03/ex1-1.c
@@ -2,35 +2,36 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     // Fixed-size array
     char fixedArray[5] = "inno";
     char fixedArray2[] = {'i', 'n', 'n', 'o', '\n'};
 
     printf("Fixed Array Before: %s\n", fixedArray);
-    
+
     int i;
-    for (i = 0; i < 5; ++i) {
+    for (i = 0; i < 5; ++i)
+    {
         printf("%c\t", fixedArray2[i]);
     }
 
     // Attempt to increase the size of the string (This is not possible)
-    fixedArray = "innopolis"; 
+    // strcat(fixedArray, "polis");
     // This line would result in an error
 
     // Dynamic-size array using malloc
-    char *dynamicArray = (char *)malloc(9 * sizeof(char)); // 9 characters for "innopolis" + null terminator
-  
-
-    // Copy the "inno" string to the dynamic array
+    char *dynamicArray = (char *)malloc(5 * sizeof(char)); // Initialize with "inno"
     strcpy(dynamicArray, "inno");
-    printf("Dynamic Array Before: %s\n", dynamicArray);
 
-    // Extend the dynamic array to "innopolis"
+    printf("Dynamic Array: %s\n", dynamicArray);
+
+    // Increase the size to hold "innopolis" using realloc
+    dynamicArray = (char *)realloc(dynamicArray, 9 * sizeof(char)); // Allocate space for "innopolis"
     strcat(dynamicArray, "polis");
-    printf("Dynamic Array After: %s\n", dynamicArray);
 
-    // Free the dynamically allocated memory
+    printf("Dynamic Array (After resizing): %s\n", dynamicArray);
+
     free(dynamicArray);
 
     return 0;
